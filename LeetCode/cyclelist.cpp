8e87cfdd72ac175467445c09ad09d8b66c6a71b1
Hardik Singh Negi <hardiksinghnegi@gmail.com>
2018-05-21 00:44:54 +0530

    Find whether linklist has a cycle.

---
+++ LeetCode/cyclelist.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head==NULL){
+            return false;
+        }
+        if(head->next == NULL){
+            return false;
+        }
+        ListNode *slow,*fast;
+        slow = head;
+        fast = head->next;
+        
+        while(fast!=slow){
+            if(fast==NULL || fast->next==NULL){
+                return false;
+            }
+            fast = fast->next->next;
+            slow = slow->next;
+            
+        }
+        
+        return true;
+    }
+};
